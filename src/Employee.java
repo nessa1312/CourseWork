e181240a4232eb34a13112662870c1238fc2bcbf
vanessa <vanesska_denis2011@mail.ru>
2022-11-04 16:07:20 +0300

    CourseWork is done

---
+++ src/Employee.java
@@ -0,0 +1,48 @@
+public class Employee {
+    private int salary;
+    private String name;
+    private int department;
+    private int ID;
+    private static int meterID;
+
+    public int getSalary(){
+        return salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getDepartment(){
+        return department;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public static int getMeterID(){
+        return meterID;
+    }
+
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
+
+    public void setDepartment (int department){
+        this.department = department;
+    }
+
+    @Override
+    public String toString(){
+        return "Name: " + name + " Salary: " + salary + " Department: " + department + " ID: " + ID;
+    }
+
+    public Employee(int salary, String name, int department) {
+        this.salary = salary;
+        this.name = name;
+        this.department = department;
+        meterID += 1;
+        this.ID = getMeterID();
+    }
+}
